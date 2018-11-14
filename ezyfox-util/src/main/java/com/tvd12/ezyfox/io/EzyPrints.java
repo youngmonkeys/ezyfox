610f38b8f0ef47b05c70ee845482902bf5414504
Dung Ta Van <itprono3@gmail.com>
2018-11-15 00:36:07 +0700

    update string printer

--- ezyfox-util/src/main/java/com/tvd12/ezyfox/io/EzyPrints.java
+++ ezyfox-util/src/main/java/com/tvd12/ezyfox/io/EzyPrints.java
@@ -3,7 +3,6 @@ package com.tvd12.ezyfox.io;
 import java.util.Arrays;
 
 public final class EzyPrints {
-
 	private final static char[] HEX_ARRAY = new char[] {
 			'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
 	};
@@ -63,6 +62,142 @@ public final class EzyPrints {
 		}
 		String answer = new String(hexChars);
 		return answer;
+	}
+	
+	public static String print2d(int[][] table) {
+		StringBuilder builder = new StringBuilder();
+		Array2DPrinter printer = new Array2DPrinter(builder);
+		printer.print(table);
+		return builder.toString();
+	}
+	
+	public static String print2d(String[][] table) {
+		StringBuilder builder = new StringBuilder();
+		Array2DPrinter printer = new Array2DPrinter(builder);
+		printer.print(table);
+		return builder.toString();
+	}
+	
+	public static final class Array2DPrinter {
+
+		private static final char BORDER_KNOT = '+';
+		private static final char HORIZONTAL_BORDER = '-';
+		private static final char VERTICAL_BORDER = '|';
+
+		private static final String DEFAULT_AS_NULL = "(NULL)";
+
+		private final StringBuilder out;
+		private final String asNull;
+
+		public Array2DPrinter(StringBuilder out) {
+			this(out, DEFAULT_AS_NULL);
+		}
+
+		public Array2DPrinter(StringBuilder out, String asNull) {
+			if (out == null) {
+				throw new IllegalArgumentException("No print stream provided");
+			}
+			if (asNull == null) {
+				throw new IllegalArgumentException("No NULL-value placeholder provided");
+			}
+			this.out = out;
+			this.asNull = asNull;
+		}
+
+		public void print(int[][] table) {
+			String[][] strss = new String[table.length][];
+			for (int i = 0; i < table.length; i++) {
+				strss[i] = new String[table[i].length];
+				for (int k = 0; k < table[i].length; k++)
+					strss[i][k] = String.valueOf(table[i][k]);
+			}
+			print(strss);
+		}
+
+		public void print(String[][] table) {
+			if (table == null) {
+				throw new IllegalArgumentException("No tabular data provided");
+			}
+			if (table.length == 0) {
+				return;
+			}
+			final int[] widths = new int[getMaxColumns(table)];
+			adjustColumnWidths(table, widths);
+			printPreparedTable(table, widths, getHorizontalBorder(widths));
+		}
+
+		private void printPreparedTable(String[][] table, int widths[], String horizontalBorder) {
+			final int lineLength = horizontalBorder.length();
+			out.append(horizontalBorder).append("\n");
+			int index = 0;
+			for (final String[] row : table) {
+				if (row != null) {
+					out.append(getRow(row, widths, lineLength))
+						.append("\n")
+						.append(horizontalBorder);
+					if((index ++) < table.length - 1)
+						out.append("\n");
+				}
+			}
+		}
+
+		private String getRow(String[] row, int[] widths, int lineLength) {
+			final StringBuilder builder = new StringBuilder(lineLength).append(VERTICAL_BORDER);
+			final int maxWidths = widths.length;
+			for (int i = 0; i < maxWidths; i++) {
+				builder.append(padRight(getCellValue(safeGet(row, i, null)), widths[i])).append(VERTICAL_BORDER);
+			}
+			return builder.toString();
+		}
+
+		private String getHorizontalBorder(int[] widths) {
+			final StringBuilder builder = new StringBuilder(256);
+			builder.append(BORDER_KNOT);
+			for (final int w : widths) {
+				for (int i = 0; i < w; i++) {
+					builder.append(HORIZONTAL_BORDER);
+				}
+				builder.append(BORDER_KNOT);
+			}
+			return builder.toString();
+		}
+
+		private int getMaxColumns(String[][] rows) {
+			int max = 0;
+			for (final String[] row : rows) {
+				if (row != null && row.length > max) {
+					max = row.length;
+				}
+			}
+			return max;
+		}
+
+		private void adjustColumnWidths(String[][] rows, int[] widths) {
+			for (final String[] row : rows) {
+				if (row != null) {
+					for (int c = 0; c < widths.length; c++) {
+						final String cv = getCellValue(safeGet(row, c, asNull));
+						final int l = cv.length();
+						if (widths[c] < l) {
+							widths[c] = l;
+						}
+					}
+				}
+			}
+		}
+
+		private static String padRight(String s, int n) {
+			return String.format("%1$-" + n + "s", s);
+		}
+
+		private static String safeGet(String[] array, int index, String defaultValue) {
+			return index < array.length ? array[index] : defaultValue;
+		}
+
+		private String getCellValue(Object value) {
+			return value == null ? asNull : value.toString();
+		}
+
 	}
 	
 }
