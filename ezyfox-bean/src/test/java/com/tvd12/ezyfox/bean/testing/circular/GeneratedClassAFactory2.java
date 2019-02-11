40bdb8109ff7b9551bae0f33ff93fd39ce77f39c
Dung Ta Van <itprono3@gmail.com>
2019-02-11 13:16:49 +0700

    update bean: test

--- ezyfox-bean/src/test/java/com/tvd12/ezyfox/bean/testing/circular/GeneratedClassAFactory2.java
+++ ezyfox-bean/src/test/java/com/tvd12/ezyfox/bean/testing/circular/GeneratedClassAFactory2.java
@@ -28,4 +28,25 @@ public class GeneratedClassAFactory2 {
 		supplier.supply(context);
 	}
 	
+	@Test
+	public void test1() throws Exception {
+		try {
+			EzyByConstructorPrototypeSupplierLoader.setDebug(true);
+			
+			EzyPrototypeSupplierLoader builder = 
+					new EzyByConstructorPrototypeSupplierLoader(new EzyClass(ClassA.class));
+			
+			EzyPrototypeSupplier supplier = builder.load(new EzySimplePrototypeFactory());
+			
+			EzyBeanContext context = EzySimpleBeanContext.builder()
+					.scan("com.tvd12.ezyfox.bean.testing.circular")
+					.build();
+			System.out.println(supplier.getObjectType());
+			supplier.supply(context);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
