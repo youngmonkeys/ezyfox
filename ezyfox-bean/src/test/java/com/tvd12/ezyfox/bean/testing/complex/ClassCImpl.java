60396ef043162d17f3bfb81fa1aa4e296259317c
Dung Ta Van <itprono3@gmail.com>
2019-02-12 16:44:32 +0700

    update bean: add more test

---
+++ ezyfox-bean/src/test/java/com/tvd12/ezyfox/bean/testing/complex/ClassCImpl.java
@@ -0,0 +1,11 @@
+package com.tvd12.ezyfox.bean.testing.complex;
+
+import com.tvd12.ezyfox.bean.annotation.EzyAutoBind;
+
+public class ClassCImpl implements ClassC {
+
+	@EzyAutoBind
+	public ClassCImpl(ClassD classD) {
+	}
+	
+}
---
+++ ezyfox-bean/src/test/java/com/tvd12/ezyfox/bean/testing/complex/ClassD.java
@@ -0,0 +1,5 @@
+package com.tvd12.ezyfox.bean.testing.complex;
+
+public interface ClassD {
+
+}
