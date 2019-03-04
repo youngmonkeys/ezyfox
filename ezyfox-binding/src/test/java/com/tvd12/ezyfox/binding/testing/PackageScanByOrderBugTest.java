4fbc5a04349fd3dd0c3511836486b21bfb4cf1ce
Dung Ta Van <itprono3@gmail.com>
2019-03-04 16:47:57 +0700

    update binding: add more testing

---
+++ ezyfox-binding/src/test/java/com/tvd12/ezyfox/binding/testing/PackageScanByOrderBugTest.java
@@ -0,0 +1,21 @@
+package com.tvd12.ezyfox.binding.testing;
+
+import org.testng.annotations.Test;
+
+import com.tvd12.ezyfox.binding.EzyBindingContext;
+import com.tvd12.ezyfox.binding.EzyMarshaller;
+import com.tvd12.ezyfox.binding.testing.data.UserData;
+
+public class PackageScanByOrderBugTest {
+
+	@Test
+	public void test() {
+		EzyBindingContext bindingContext = EzyBindingContext.builder()
+				.scan("com.tvd12.ezyfox.binding.testing.data")
+				.scan("com.tvd12.ezyfox.binding.testing.ext.data")
+				.build();
+		EzyMarshaller marshaller = bindingContext.newMarshaller();
+		marshaller.marshal(new UserData());
+	}
+	
+}
---
+++ ezyfox-binding/src/test/java/com/tvd12/ezyfox/binding/testing/data/UserData.java
@@ -0,0 +1,8 @@
+package com.tvd12.ezyfox.binding.testing.data;
+
+import com.tvd12.ezyfox.binding.annotation.EzyObjectBinding;
+
+@EzyObjectBinding
+public class UserData {
+
+}
