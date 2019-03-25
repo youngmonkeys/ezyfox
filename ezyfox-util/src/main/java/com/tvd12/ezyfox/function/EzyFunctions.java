97b9a669505a727a83251781aa84a8b5a63a898d
Dung Ta Van <itprono3@gmail.com>
2019-03-25 10:55:44 +0700

    add functions

---
+++ ezyfox-util/src/main/java/com/tvd12/ezyfox/function/EzyFunctions.java
@@ -0,0 +1,14 @@
+package com.tvd12.ezyfox.function;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Function;
+
+public final class EzyFunctions {
+
+	public static final Function<Object, Lock> NEW_REENTRANT_LOCK_FUNC = p -> new ReentrantLock();
+	
+	private EzyFunctions() {
+	}
+	
+}
