7b9b2da2160ac2994c2b820e067ed0abba949465
Dung Ta Van <itprono3@gmail.com>
2019-03-30 14:18:57 +0700

    update bean: fix set properties bug

--- ezyfox-bean/src/test/java/com/tvd12/ezyfox/bean/testing/prototype/ClassA.java
+++ ezyfox-bean/src/test/java/com/tvd12/ezyfox/bean/testing/prototype/ClassA.java
@@ -1,16 +1,59 @@
 package com.tvd12.ezyfox.bean.testing.prototype;
 
+import com.tvd12.ezyfox.annotation.EzyProperty;
 import com.tvd12.ezyfox.bean.annotation.EzyAutoBind;
 import com.tvd12.ezyfox.bean.annotation.EzyPostInit;
 import com.tvd12.ezyfox.bean.annotation.EzyPrototype;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
+@ToString
 @EzyPrototype
 public class ClassA {
 
+	@Setter
+	@EzyProperty("game.auto_start")
+	private boolean autoStart;
+	@EzyProperty("game.auto_start")
+	public boolean autoStartField;
+	@Setter
+	@EzyProperty("game.byte")
+	private byte byteValue;
+	@EzyProperty("game.byte")
+	public byte byteValueField;
+	@Setter
+	@EzyProperty("game.char")
+	private char charValue;
+	@EzyProperty("game.char")
+	public char charValueField;
+	@Setter
+	@EzyProperty("game.double")
+	private double doubleValue;
+	@EzyProperty("game.double")
+	public double doubleValueField;
+	@Setter
+	@EzyProperty("game.float")
+	private float floatValue;
+	@EzyProperty("game.float")
+	public float floatValueField;
+	@Setter
+	@EzyProperty("game.int")
+	private int intValue;
+	@EzyProperty("game.int")
+	public int intValueField;
+	@Setter
+	@EzyProperty("game.long")
+	private long longValue;
+	@EzyProperty("game.long")
+	public long longValueField;
+	@Setter
+	@EzyProperty("game.short")
+	private short shortValue;
+	@EzyProperty("game.short")
+	public short shortValueField;
 	private final ClassB classB;
 	private final ClassD classD;
 	
