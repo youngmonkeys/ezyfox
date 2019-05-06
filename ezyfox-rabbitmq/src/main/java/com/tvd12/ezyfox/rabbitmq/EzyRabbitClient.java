b15856842419bbcd76022b1d27def613dd85f293
Dung Ta Van <itprono3@gmail.com>
2019-05-06 22:32:19 +0700

    update rabbitmq

--- ezyfox-rabbitmq/src/main/java/com/tvd12/ezyfox/rabbitmq/EzyRabbitClient.java
+++
@@ -1,9 +0,0 @@
-package com.tvd12.ezyfox.rabbitmq;
-
-import com.tvd12.ezyfox.util.EzyShutdownable;
-
-public interface EzyRabbitClient extends EzyShutdownable {
-	
-	void send(String cmd, Object data);
-	
-}
--- ezyfox-rabbitmq/src/main/java/com/tvd12/ezyfox/rabbitmq/EzyRabbitServer.java
+++
@@ -1,7 +0,0 @@
-package com.tvd12.ezyfox.rabbitmq;
-
-import com.tvd12.ezyfox.util.EzyShutdownable;
-import com.tvd12.ezyfox.util.EzyStartable;
-
-public interface EzyRabbitServer extends EzyStartable, EzyShutdownable {
-}
