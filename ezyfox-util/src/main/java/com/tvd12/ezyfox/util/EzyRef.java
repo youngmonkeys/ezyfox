9315c22e4a51062ecd6e9690e3092cf6e6d4122e
Dung Ta Van <itprono3@gmail.com>
2019-06-04 23:54:49 +0700

    add Ref

---
+++ ezyfox-util/src/main/java/com/tvd12/ezyfox/util/EzyRef.java
@@ -0,0 +1,29 @@
+package com.tvd12.ezyfox.util;
+
+public class EzyRef<T> {
+
+	protected final T value;
+	protected volatile int referenceCount;
+	
+	public EzyRef(T value) {
+		this.value = value;
+	}
+	
+	public T get() {
+		return value;
+	}
+	
+	public void retain() {
+		++ this.referenceCount;
+	}
+	
+	public void release() {
+		-- this.referenceCount;
+	}
+	
+	public boolean isReleasable() {
+		return this.referenceCount == 0;
+	}
+	
+	
+}
