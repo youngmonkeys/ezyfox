b72546f2530f98ff66c28e543b2214913a4f1468
Dung Ta Van <itprono3@gmail.com>
2019-06-08 23:20:22 +0700

    test loggable

--- ezyfox-util/src/test/java/com/tvd12/ezyfox/testing/util/EzyLoggableTest.java
+++ ezyfox-util/src/test/java/com/tvd12/ezyfox/testing/util/EzyLoggableTest.java
@@ -3,12 +3,32 @@ package com.tvd12.ezyfox.testing.util;
 import org.testng.annotations.Test;
 
 import com.tvd12.ezyfox.util.EzyLoggable;
+import com.tvd12.test.performance.Performance;
 
 public class EzyLoggableTest extends EzyLoggable {
 
 	@Test
 	public void test() {
 		getLogger().debug("abc");
+	}
+	
+	@Test
+	public void performanceTest() {
+		long time1 = Performance.create()
+				.test(A::new)
+				.getTime();
+		long time2 = Performance.create()
+				.test(B::new)
+				.getTime();
+		System.out.println("EzyLoggableTest::performanceTest: time1: " + time1 + ", time2: " + time2);
+	}
+	
+	public static class A extends EzyLoggable {
+		
+	}
+	
+	public static class B {
+		
 	}
 	
 }
