837d3542466ba34869c9cfbeb298cee557278197
Dung Ta Van <itprono3@gmail.com>
2019-06-08 17:44:37 +0700

    add binding codec

--- build.bat
+++ build.bat
@@ -7,6 +7,7 @@ mvn clean install -Dmaven.test.skip=true -f ezyfox-security & ^
 mvn clean install -Dmaven.test.skip=true -f ezyfox-common & ^
 mvn clean install -Dmaven.test.skip=true -f ezyfox-codec & ^
 mvn clean install -Dmaven.test.skip=true -f ezyfox-binding & ^
+mvn clean install -Dmaven.test.skip=true -f ezyfox-binding-codec & ^
 mvn clean install -Dmaven.test.skip=true -f ezyfox-bean & ^
 mvn clean install -Dmaven.test.skip=true -f ezyfox-json & ^
 mvn clean install -Dmaven.test.skip=true -f ezyfox-mapping & ^
---
+++ ezyfox-binding-codec/src/test/java/com/tvd12/ezyfox/binding/codec/test/EzyBindingEntityCodecTest.java
@@ -0,0 +1,44 @@
+package com.tvd12.ezyfox.binding.codec.test;
+
+import org.testng.annotations.Test;
+
+import com.tvd12.ezyfox.binding.EzyBindingContext;
+import com.tvd12.ezyfox.binding.codec.EzyBindingEntityCodec;
+import com.tvd12.ezyfox.codec.EzyEntityCodec;
+import com.tvd12.ezyfox.codec.MsgPackSimpleDeserializer;
+import com.tvd12.ezyfox.codec.MsgPackSimpleSerializer;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+public class EzyBindingEntityCodecTest {
+
+	@Test
+	public void test() {
+		EzyBindingContext bindingContext = EzyBindingContext.builder()
+				.addClass(A.class)
+				.build();
+		EzyEntityCodec codec = EzyBindingEntityCodec.builder()
+				.marshaller(bindingContext.newMarshaller())
+				.unmarshaller(bindingContext.newUnmarshaller())
+				.messageSerializer(new MsgPackSimpleSerializer())
+				.messageDeserializer(new MsgPackSimpleDeserializer())
+				.build();
+		byte[] bytes = codec.serialize(new A("hello"));
+		A a = codec.deserialize(bytes, A.class);
+		assert a.getValue().equals("hello");
+	}
+	
+	@Getter
+	@Setter
+	@ToString
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class A {
+		protected String value;
+	}
+	
+}
