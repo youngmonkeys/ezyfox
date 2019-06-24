ba93d4a79f2209ab9b70bf7d7611775558d1d59b
Dung Ta Van <itprono3@gmail.com>
2019-06-24 22:43:14 +0700

    add UnimplementedOperationException

---
+++ ezyfox-util/src/main/java/com/tvd12/ezyfox/exception/UnimplementedOperationException.java
@@ -0,0 +1,10 @@
+package com.tvd12.ezyfox.exception;
+
+public class UnimplementedOperationException extends RuntimeException {
+	private static final long serialVersionUID = 7603927099427415774L;
+	
+	public UnimplementedOperationException(String msg, Throwable e) {
+		super(msg, e);
+	}
+
+}
---
+++ ezyfox-util/src/test/java/com/tvd12/ezyfox/testing/exception/UnimplementedOperationExceptionTest.java
@@ -0,0 +1,14 @@
+package com.tvd12.ezyfox.testing.exception;
+
+import org.testng.annotations.Test;
+
+import com.tvd12.ezyfox.exception.UnimplementedOperationException;
+
+public class UnimplementedOperationExceptionTest {
+
+	@Test
+	public void test() {
+		new UnimplementedOperationException("", new Exception());
+	}
+	
+}
