dbdc559b456ba294752d6a410ac831737da8c732
Dung Ta Van <itprono3@gmail.com>
2019-06-28 22:13:50 +0700

    update assembly plugin

---
+++ ezyfox-tools/src/test/java/com/tvd12/ezyfox/testing/tool/ChangeAssemblyConfigTool.java
@@ -0,0 +1,29 @@
+package com.tvd12.ezyfox.testing.tool;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChangeAssemblyConfigTool {
+
+	public static void main(String[] args) throws IOException {
+		Path path = Paths.get("/Users/tavandung12/Documents/tvd12/java/projects/ezyfox");
+		List<Path> paths = new ArrayList<>();
+		Files.walk(path)
+			.filter(p -> p.toString().endsWith("assembly.xml"))
+			.forEach(p -> {
+				paths.add(p);
+			});
+		for(Path p : paths) {
+			byte[] bytes = Files.readAllBytes(p);
+			String str = new String(bytes);
+			String newStr = str.replace("<format>zip</format>", "<format>dir</format>");
+			Files.write(p, newStr.getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
+		}
+	}
+	
+}
