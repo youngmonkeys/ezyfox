fb208298d2ed78d9ab9b5d193e26731f1b672a13
Dung Ta Van <itprono3@gmail.com>
2019-07-22 20:01:21 +0700

    update dependencies, remove common-lang3

---
+++ ezyfox-concurrent/src/main/java/com/tvd12/ezyfox/concurrent/LazyInitializer.java
@@ -0,0 +1,23 @@
+package com.tvd12.ezyfox.concurrent;
+
+public abstract class LazyInitializer<T> {
+
+	protected T object;
+	
+	public T get() {
+		if(object == null)
+			return synGet();
+		return object;
+	}
+	
+	protected T synGet() {
+		synchronized (this) {
+			if(object == null)
+				object = initialize();
+		}
+		return object;
+	}
+
+	protected abstract T initialize();
+	
+}
