36283eddecc2b749fb2d6a6fdff8f4c8a4a0b31b
Dung Ta Van <itprono3@gmail.com>
2020-03-15 10:38:16 +0700

    add round robin

---
+++ ezyfox-util/src/main/java/com/tvd12/ezyfox/util/EzyRoundRobin.java
@@ -0,0 +1,44 @@
+package com.tvd12.ezyfox.util;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class EzyRoundRobin<T> {
+
+	protected final Queue<T> queue;
+	
+	public EzyRoundRobin() {
+		this.queue = new LinkedList<>();
+	}
+	
+	public EzyRoundRobin(Supplier<T> supplier, int size) {
+		this();
+		for(int i = 0 ; i < size ; ++i)
+			this.queue.offer(supplier.get());
+	}
+	
+	public void add(T item) {
+		synchronized (queue) {
+			this.queue.add(item);
+		}
+	}
+	
+	public T get() {
+		T last = null;
+		synchronized (queue) {
+			last = queue.poll();
+			queue.offer(last);
+		}
+		return last;
+	}
+
+	public void forEach(Consumer<T> consumer) {
+		synchronized (queue) {
+			for(T item : queue)
+				consumer.accept(item);
+		}
+	}
+	
+}
---
+++ ezyfox-util/src/test/java/com/tvd12/ezyfox/testing/util/EzyRoundRobinTest.java
@@ -0,0 +1,33 @@
+package com.tvd12.ezyfox.testing.util;
+
+import org.testng.annotations.Test;
+
+import com.tvd12.ezyfox.util.EzyRoundRobin;
+import com.tvd12.test.performance.Performance;
+
+public class EzyRoundRobinTest {
+
+	@Test
+	public void test() {
+		EzyRoundRobin<Integer> rr = new EzyRoundRobin<>(() -> 1, 1);
+		rr.add(2);
+		rr.add(3);
+		rr.forEach(i -> System.out.println(i));
+		assert rr.get().equals(1);
+		assert rr.get().equals(2);
+		assert rr.get().equals(3);
+		assert rr.get().equals(1);
+		assert rr.get().equals(2);
+		assert rr.get().equals(3);
+		assert rr.get().equals(1);
+		assert rr.get().equals(2);
+		assert rr.get().equals(3);
+		
+		long time = Performance.create()
+			.test(() -> {
+				rr.get();
+			}).getTime();
+		System.out.println("time: " + time);
+	}
+	
+}
