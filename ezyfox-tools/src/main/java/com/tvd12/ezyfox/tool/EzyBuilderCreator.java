1a19c78bf20815610523cfef16d34641957d8df6
Dung Ta Van <itprono3@gmail.com>
2020-07-27 22:41:48 +0700

    update tools

--- ezyfox-tools/src/main/java/com/tvd12/ezyfox/tool/EzyBuilderCreator.java
+++ ezyfox-tools/src/main/java/com/tvd12/ezyfox/tool/EzyBuilderCreator.java
@@ -6,15 +6,29 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.tvd12.ezyfox.reflect.EzyField;
+
 public class EzyBuilderCreator {
+	
+	protected boolean buildBySetter;
+	
+	public EzyBuilderCreator buildBySetter(boolean buildBySetter) {
+		this.buildBySetter = buildBySetter;
+		return this;
+	}
 
 	public String create(Class<?> clazz) throws Exception {
+		return create(clazz, "Builder");
+	}
+	
+	public String create(
+			Class<?> clazz, String builderClassName) throws Exception {
 		StringBuilder classScript = new StringBuilder();
 		StringBuilder builderScript = new StringBuilder();
 		classScript.append("public class ")
 			.append(clazz.getSimpleName())
 			.append(" {\n\n");
-		builderScript.append("public static class Builder implements EzyBuilder<")
+		builderScript.append("public static class " + builderClassName + " implements EzyBuilder<")
 				.append(clazz.getSimpleName())
 			.append("> {\n\n");
 		List<Field> fields = Arrays.stream(clazz.getDeclaredFields())
@@ -53,7 +67,7 @@ public class EzyBuilderCreator {
 		}
 		builderScript.append("\n");
 		for(Field field : fields) {
-			builderScript.append("\tpublic Builder ")
+			builderScript.append("\tpublic " + builderClassName + " ")
 				.append(field.getName())
 				.append("(")
 					.append(field.getType().getSimpleName())
@@ -65,14 +79,28 @@ public class EzyBuilderCreator {
 				.append("\n\t}\n\n");
 		}
 		builderScript.append("\tpublic ").append(clazz.getSimpleName())
-			.append(" build() {\n")
-				.append("\t\treturn new ").append(clazz.getSimpleName()).append("(this);\n")
-			.append("\t}"); 
+			.append(" build() {\n");
+		if(buildBySetter) {
+			builderScript.append("\t\t").append(clazz.getSimpleName())
+				.append(" p = new ")
+				.append(clazz.getSimpleName()).append("();\n");
+			for(Field field : fields) {
+				EzyField f = new EzyField(field);
+				builderScript.append("\t\tp.").append(f.getSetterMethod())
+					.append("(").append(f.getName()).append(");")
+					.append("\n");
+			}
+			builderScript.append("\t\treturn p;\n");
+		}
+		else {
+			builderScript.append("\t\treturn new ").append(clazz.getSimpleName()).append("(this);\n");
+		}
+		builderScript.append("\t}"); 
 		builderScript.append("\n\n}");
 		return classScript
 				.append("\n\n")
-				.append("\tpublic static Builder builder() {\n")
-				.append("\t\treturn new Builder();\n")
+				.append("\tpublic static " + builderClassName + " builder() {\n")
+				.append("\t\treturn new " + builderClassName + "();\n")
 				.append("\t}\n\n")
 				.append(EzyStringTool.tabAll(builderScript.toString(), 1))
 				.append("\n\n")
