d16f29bad849d760f8e6a2df5603b1e1c95a4d86
Dung Ta Van <itprono3@gmail.com>
2021-05-16 15:35:18 +0700

    update bean: auto config

---
+++ ezyfox-bean/src/test/java/com/tvd12/ezyfox/bean/testing/EzyBeanAutoConfigTest.java
@@ -0,0 +1,32 @@
+package com.tvd12.ezyfox.bean.testing;
+
+import org.testng.annotations.Test;
+
+import com.tvd12.ezyfox.bean.EzyBeanAutoConfig;
+
+public class EzyBeanAutoConfigTest {
+
+	@Test
+	public void testSuccess() {
+		new A().config();
+	}
+	
+	@Test
+	public void testException() {
+		new B().config();
+	}
+	
+	private static class A implements EzyBeanAutoConfig {
+		@Override
+		public void autoConfig() {
+		}
+	}
+	
+	private static class B implements EzyBeanAutoConfig {
+		@Override
+		public void autoConfig() {
+			throw new IllegalStateException();
+		}
+	}
+	
+}
