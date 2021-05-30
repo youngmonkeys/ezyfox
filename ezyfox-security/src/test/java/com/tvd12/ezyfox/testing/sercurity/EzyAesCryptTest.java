982c6885a4a64ec01e250968389f92869e432ce0
Dung Ta Van <itprono3@gmail.com>
2021-05-30 17:22:42 +0700

    update security: add aes

---
+++ ezyfox-security/src/test/java/com/tvd12/ezyfox/testing/sercurity/EzyAesCryptTest.java
@@ -0,0 +1,45 @@
+package com.tvd12.ezyfox.testing.sercurity;
+
+import org.testng.annotations.Test;
+
+import com.tvd12.ezyfox.sercurity.EzyAesCrypt;
+import com.tvd12.test.assertion.Asserts;
+
+public class EzyAesCryptTest {
+
+	@Test
+	public void test() throws Exception {
+		// given
+		byte[] key = EzyAesCrypt.randomKey();
+        byte[] clean = "Quisque eget odio ac lectus vestibulum faucibus eget.".getBytes();
+        
+        // when
+        EzyAesCrypt sut = EzyAesCrypt.builder()
+        		.build();
+        byte[] encrypted = sut.encrypt(clean, key);
+        byte[] decrypted = sut.decrypt(encrypted, key);
+        
+        // then
+        Asserts.assertEquals(clean, decrypted);
+	}
+	
+	@Test
+	public void testMinSize() throws Exception {
+		// given
+		byte[] key = EzyAesCrypt.randomKey(16);
+        byte[] clean = "Quisque eget odio ac lectus vestibulum faucibus eget.".getBytes();
+        
+        // when
+        EzyAesCrypt sut = EzyAesCrypt.builder()
+        		.initVectorSize(16)
+        		.keySpecAlgorithm("AES")
+        		.transformation("AES/CBC/PKCS5Padding")
+        		.build();
+        byte[] encrypted = sut.encrypt(clean, key);
+        byte[] decrypted = sut.decrypt(encrypted, key);
+        
+        // then
+        Asserts.assertEquals(clean, decrypted);
+	}
+	
+}
--- ezyfox-security/src/test/java/com/tvd12/ezyfox/testing/sercurity/EzyAsyCryptTesting.java
+++ ezyfox-security/src/test/java/com/tvd12/ezyfox/testing/sercurity/EzyAsyCryptTesting.java
@@ -6,9 +6,11 @@ import java.security.KeyPair;
 import org.testng.annotations.Test;
 
 import com.tvd12.ezyfox.file.EzySimpleFileWriter;
+import com.tvd12.ezyfox.sercurity.EzyAesCrypt;
 import com.tvd12.ezyfox.sercurity.EzyAsyCrypt;
 import com.tvd12.ezyfox.sercurity.EzyBase64;
 import com.tvd12.ezyfox.sercurity.EzyFileKeysGenerator;
+import com.tvd12.test.assertion.Asserts;
 import com.tvd12.test.base.BaseTest;
 
 public class EzyAsyCryptTesting extends BaseTest {
@@ -41,7 +43,46 @@ public class EzyAsyCryptTesting extends BaseTest {
 		String decryptedText = asyCrypt.decrypt(EzyBase64.decode(encryptedText), String.class);
 		
 		System.out.println("decryptedText = " + decryptedText);
+	}
+	
+	@Test
+	public void clearTest() throws Exception {
+		EzyFileKeysGenerator keysGenerator = EzyFileKeysGenerator.builder()
+				.algorithm("RSA")
+				.keysize(2048)
+				.publicKeyFile(new File("output/publickey.txt"))
+				.privateKeyFile(new File("output/privatekey.txt"))
+				.fileWriter(EzySimpleFileWriter.builder().build())
+				.build();
+		KeyPair keyPair = keysGenerator.generate();
 		
+		EzyAsyCrypt asyCrypt = EzyAsyCrypt.builder()
+				.algorithm("RSA")
+				.publicKey(keyPair.getPublic().getEncoded())
+				.build();
+		
+		byte[] text = EzyAesCrypt.randomKey();
+		byte[] encryptedText = asyCrypt.encrypt(text);
+		
+		System.out.println("text = " + text.length);
+		System.out.println("encryptedText = " + encryptedText.length);
+		
+		asyCrypt = EzyAsyCrypt.builder()
+				.privateKey(keyPair.getPrivate().getEncoded())
+				.build();
+		byte[] decryptedText = asyCrypt.decrypt(encryptedText);
+		
+		byte[] data = "Hello".getBytes();
+		
+		byte[] cdata = EzyAesCrypt.builder()
+				.build()
+				.encrypt(data, text);
+		byte[] ddata = EzyAesCrypt.builder()
+				.build()
+				.decrypt(cdata, text);
+		Asserts.assertEquals(data, ddata);
+		
+		System.out.println("decryptedText = " + new String(decryptedText));
 	}
 	
 }
