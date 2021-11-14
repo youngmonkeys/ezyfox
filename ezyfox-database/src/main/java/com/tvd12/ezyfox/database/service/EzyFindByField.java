a99041add3e48f084b79ab24a06f3465218837b3
Dung Ta Van <itprono3@gmail.com>
2021-11-14 20:35:27 +0700

    update database

--- ezyfox-database/src/main/java/com/tvd12/ezyfox/database/service/EzyFindByField.java
+++ ezyfox-database/src/main/java/com/tvd12/ezyfox/database/service/EzyFindByField.java
@@ -1,7 +1,13 @@
 package com.tvd12.ezyfox.database.service;
 
+import java.util.Optional;
+
 public interface EzyFindByField<E> {
 
 	E findByField(String field, Object value);
+	
+	default Optional<E> findByFieldOptional(String field, Object value) {
+	    return Optional.ofNullable(findByField(field, value));
+	}
 	
 }
--- ezyfox-database/src/main/java/com/tvd12/ezyfox/database/service/EzyFindById.java
+++ ezyfox-database/src/main/java/com/tvd12/ezyfox/database/service/EzyFindById.java
@@ -1,7 +1,13 @@
 package com.tvd12.ezyfox.database.service;
 
+import java.util.Optional;
+
 public interface EzyFindById<I,E> {
 
 	E findById(I id);
+	
+	default Optional<E> findByIdOptional(I id) {
+	    return Optional.ofNullable(findById(id));
+	}
 	
 }
