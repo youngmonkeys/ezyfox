7850424232ec56ed3ff78d1eaf1f780c48bb58d5
Dung Ta Van <itprono3@gmail.com>
2022-04-24 10:25:03 +0700

    checkstyle

--- ezyfox-binding-codec/src/test/java/com/tvd12/ezyfox/binding/codec/test/EzyBindingEntityCodecTest.java
+++ ezyfox-binding-codec/src/test/java/com/tvd12/ezyfox/binding/codec/test/EzyBindingEntityCodecTest.java
@@ -1,32 +1,26 @@
 package com.tvd12.ezyfox.binding.codec.test;
 
-import org.testng.annotations.Test;
-
 import com.tvd12.ezyfox.binding.EzyBindingContext;
 import com.tvd12.ezyfox.binding.codec.EzyBindingEntityCodec;
 import com.tvd12.ezyfox.codec.EzyEntityCodec;
 import com.tvd12.ezyfox.codec.MsgPackSimpleDeserializer;
 import com.tvd12.ezyfox.codec.MsgPackSimpleSerializer;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
+import org.testng.annotations.Test;
 
 public class EzyBindingEntityCodecTest {
 
     @Test
     public void test() {
         EzyBindingContext bindingContext = EzyBindingContext.builder()
-                .addClass(A.class)
-                .build();
+            .addClass(A.class)
+            .build();
         EzyEntityCodec codec = EzyBindingEntityCodec.builder()
-                .marshaller(bindingContext.newMarshaller())
-                .unmarshaller(bindingContext.newUnmarshaller())
-                .messageSerializer(new MsgPackSimpleSerializer())
-                .messageDeserializer(new MsgPackSimpleDeserializer())
-                .build();
+            .marshaller(bindingContext.newMarshaller())
+            .unmarshaller(bindingContext.newUnmarshaller())
+            .messageSerializer(new MsgPackSimpleSerializer())
+            .messageDeserializer(new MsgPackSimpleDeserializer())
+            .build();
         byte[] bytes = codec.serialize(new A("hello"));
         A a = codec.deserialize(bytes, A.class);
         assert a.getValue().equals("hello");
