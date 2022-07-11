a205c70535962759d691b22761a06ff17eb0ef77
Dung Ta Van <itprono3@gmail.com>
2022-07-11 22:20:34 +0700

    ezyfox-bean: fix did not bind value to @Property annotation field in prototype

--- ezyfox-bean/src/test/java/com/tvd12/ezyfox/bean/testing/singleton/ClassA.java
+++ ezyfox-bean/src/test/java/com/tvd12/ezyfox/bean/testing/singleton/ClassA.java
@@ -1,11 +1,17 @@
 package com.tvd12.ezyfox.bean.testing.singleton;
 
 import com.tvd12.ezyfox.bean.annotation.EzySingleton;
+import com.tvd12.properties.file.annotation.Property;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @EzySingleton("a")
 public class ClassA {
+
+    @Setter
+    @Property("hello_world")
+    private String hello;
 
     private final String value1 = "1";
 }
--- ezyfox-util/src/main/java/com/tvd12/ezyfox/util/EzyPropertyAnnotations.java
+++ ezyfox-util/src/main/java/com/tvd12/ezyfox/util/EzyPropertyAnnotations.java
@@ -5,12 +5,18 @@ import com.tvd12.ezyfox.reflect.EzyClass;
 import com.tvd12.ezyfox.reflect.EzyField;
 import com.tvd12.ezyfox.reflect.EzyMethod;
 import com.tvd12.ezyfox.reflect.EzyReflectElement;
+import com.tvd12.properties.file.annotation.Property;
+
+import static com.tvd12.ezyfox.io.EzyStrings.isEmpty;
 
 public final class EzyPropertyAnnotations {
 
     private EzyPropertyAnnotations() {}
 
-    public static String getPropertyName(EzyClass clazz, EzyReflectElement element) {
+    public static String getPropertyName(
+        EzyClass clazz,
+        EzyReflectElement element
+    ) {
         if (element instanceof EzyField) {
             return getPropertyName((EzyField) element);
         }
@@ -18,20 +24,45 @@ public final class EzyPropertyAnnotations {
     }
 
     public static String getPropertyName(EzyField field) {
-        EzyProperty annotation = field.getAnnotation(EzyProperty.class);
-        if (annotation.value().length() > 0) {
-            return annotation.value();
+        String propertyName = null;
+        Property propertyAnno = field.getAnnotation(Property.class);
+        if (propertyAnno != null) {
+            propertyName = propertyAnno.value();
         }
-        return field.getName();
+        if (isEmpty(propertyName)) {
+            EzyProperty ezyPropertyAnno =
+                field.getAnnotation(EzyProperty.class);
+            if (ezyPropertyAnno != null) {
+                propertyName = ezyPropertyAnno.value();
+            }
+        }
+        if (isEmpty(propertyName)) {
+            propertyName = field.getName();
+        }
+        return propertyName;
     }
 
-    public static String getPropertyName(EzyClass clazz, EzyMethod method) {
-        EzyProperty annotation = method.getAnnotation(EzyProperty.class);
-        if (annotation != null && annotation.value().length() > 0) {
-            return annotation.value();
+    public static String getPropertyName(
+        EzyClass clazz,
+        EzyMethod method
+    ) {
+        String propertyName = null;
+        Property propertyAnno = method.getAnnotation(Property.class);
+        if (propertyAnno != null) {
+            propertyName = propertyAnno.value();
         }
-        String fieldName = method.getFieldName();
-        EzyField field = clazz.getField(fieldName);
-        return field != null ? getPropertyName(field) : fieldName;
+        if (isEmpty(propertyName)) {
+            EzyProperty ezyPropertyAnno =
+                method.getAnnotation(EzyProperty.class);
+            if (ezyPropertyAnno != null) {
+                propertyName = ezyPropertyAnno.value();
+            }
+        }
+        if (isEmpty(propertyName)) {
+            String fieldName = method.getFieldName();
+            EzyField field = clazz.getField(fieldName);
+            propertyName = field != null ? getPropertyName(field) : fieldName;
+        }
+        return propertyName;
     }
 }
