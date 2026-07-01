e85b8a230e88392d67bbf20acd89697d48f92afa
tvd12 <ta.van.dung@youngmonkeys.org>
2026-07-01 14:30:24 +0700

    add test

--- ezyfox-binding/src/test/java/com/tvd12/ezyfox/binding/testing/EzyAbstractBuilderTest.java
+++ ezyfox-binding/src/test/java/com/tvd12/ezyfox/binding/testing/EzyAbstractBuilderTest.java
@@ -2,15 +2,21 @@ package com.tvd12.ezyfox.binding.testing;
 
 import com.tvd12.ezyfox.binding.EzyAccessType;
 import com.tvd12.ezyfox.binding.impl.EzyAbstractBuilder;
+import com.tvd12.ezyfox.binding.impl.EzyElementsFetcher;
 import com.tvd12.ezyfox.binding.impl.EzyObjectElementsFetcher;
 import com.tvd12.ezyfox.reflect.*;
 import com.tvd12.test.base.BaseTest;
 import org.testng.annotations.Test;
 
 import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertSame;
+import static org.testng.Assert.assertTrue;
 
 public class EzyAbstractBuilderTest extends BaseTest {
 
@@ -23,12 +29,47 @@ public class EzyAbstractBuilderTest extends BaseTest {
         builder.getElementType(null);
     }
 
+    @Test
+    @SuppressWarnings("unchecked")
+    public void setElementsByClassCacheWithAbstractElementsFetcher() {
+        // given
+        EzyElementBuilder builder = new EzyElementBuilder(new EzyClass(A.class));
+        Map<Class<?>, Map<Class<?>, List<Object>>> cache = new HashMap<>();
+
+        // when
+        builder.setElementsByClassCache(cache);
+        List<Object> elements = builder.getElements();
+
+        // then
+        Map<Class<?>, List<Object>> elementsByClass = cache.get(
+            builder.getElementsFetcherClass()
+        );
+        assertTrue(elementsByClass.containsKey(A.class));
+        assertSame(elementsByClass.get(A.class), elements);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void setElementsByClassCacheWithNonAbstractElementsFetcher() {
+        // given
+        NonAbstractElementsFetcherBuilder builder =
+            new NonAbstractElementsFetcherBuilder(new EzyClass(A.class));
+        Map<Class<?>, Map<Class<?>, List<Object>>> cache = new HashMap<>();
+
+        // when
+        builder.setElementsByClassCache(cache);
+        List<Object> elements = builder.getElements();
+
+        // then
+        assertTrue(cache.isEmpty());
+        assertTrue(elements.isEmpty());
+    }
+
     public static class A {
         public String name;
     }
 
-    @SuppressWarnings("rawtypes")
-    public static class EzyElementBuilder extends EzyAbstractBuilder {
+    public static class EzyElementBuilder extends EzyAbstractBuilder<EzyMethod> {
 
         public EzyElementBuilder(EzyClass clazz) {
             super(clazz);
@@ -76,5 +117,36 @@ public class EzyAbstractBuilderTest extends BaseTest {
             return super.getFieldName(element);
         }
 
+        @Override
+        public List<Object> getElements() {
+            return super.getElements();
+        }
+
+        public Class<?> getElementsFetcherClass() {
+            return elementsFetcher.getClass();
+        }
+    }
+
+    public static class NonAbstractElementsFetcherBuilder
+        extends EzyAbstractBuilder<EzyMethod> {
+
+        public NonAbstractElementsFetcherBuilder(EzyClass clazz) {
+            super(clazz);
+        }
+
+        @Override
+        protected int getAccessType(EzyClass clazz) {
+            return EzyAccessType.ALL;
+        }
+
+        @Override
+        protected EzyElementsFetcher newElementsFetcher() {
+            return (clazz, accessType) -> Collections.emptyList();
+        }
+
+        @Override
+        public List<Object> getElements() {
+            return super.getElements();
+        }
     }
 }
