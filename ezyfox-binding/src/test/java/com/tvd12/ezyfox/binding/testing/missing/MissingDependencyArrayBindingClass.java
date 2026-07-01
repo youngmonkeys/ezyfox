194e813115e4c61e12384006ce78b83216d9391a
tvd12 <ta.van.dung@youngmonkeys.org>
2026-07-01 14:28:06 +0700

    add test

---
+++ ezyfox-binding/src/test/java/com/tvd12/ezyfox/binding/testing/missing/MissingDependencyArrayBindingClass.java
@@ -0,0 +1,8 @@
+package com.tvd12.ezyfox.binding.testing.missing;
+
+import com.tvd12.ezyfox.binding.annotation.EzyArrayBinding;
+
+@EzyArrayBinding
+public class MissingDependencyArrayBindingClass {
+    public MissingObjectBindingDependency dependency;
+}
---
+++ ezyfox-binding/src/test/java/com/tvd12/ezyfox/binding/testing/missing/MissingDependencyObjectBindingClass.java
@@ -0,0 +1,8 @@
+package com.tvd12.ezyfox.binding.testing.missing;
+
+import com.tvd12.ezyfox.binding.annotation.EzyObjectBinding;
+
+@EzyObjectBinding
+public class MissingDependencyObjectBindingClass {
+    public MissingObjectBindingDependency dependency;
+}
